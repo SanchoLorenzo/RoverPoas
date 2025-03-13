7ca93d17b5196c0749fc5bdcb3a129e132e036da
SanchoLorenzo <lorenzo7sancho@gmail.com>
2025-03-13 15:28:16 -0600

    quite los command del joystick del setup

--- Codigos-2doPrototipo/Arduino_Centro_Control_Tx/Arduino_Centro_Control_Tx.ino
+++ Codigos-2doPrototipo/Arduino_Centro_Control_Tx/Arduino_Centro_Control_Tx.ino
@@ -22,12 +22,6 @@
 #define UP_THRESHOLD    400
 #define DOWN_THRESHOLD  800
 
-#define COMMAND_NO     0x00
-#define COMMAND_LEFT   0x01
-#define COMMAND_RIGHT  0x02
-#define COMMAND_UP     0x04
-#define COMMAND_DOWN   0x08
-
 int xValue = 0 ; // To store value of the X axis
 int yValue = 0 ; // To store value of the Y axis
 int command = COMMAND_NO;
